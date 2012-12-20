0049e26033ed16b24d35baa6a2de995ce574f078
Richard Petri <dasricht@gmail.com>
2012-12-20 13:46:13 +0100

    bison: switch to the gz archive to avoid xz dependancy

--- bison.rb
+++ bison.rb
@@ -2,11 +2,9 @@ require 'formula'
 
 class Bison < Formula
   homepage 'http://www.gnu.org/software/bison/'
-  url 'http://ftpmirror.gnu.org/bison/bison-2.7.tar.xz'
-  mirror 'http://ftp.gnu.org/gnu/bison/bison-2.7.tar.xz'
-  sha1 '245467c56663ec80fc3e80c38cc1ff18c59d7ecc'
-
-  depends_on 'xz' => :build
+  url 'http://ftpmirror.gnu.org/bison/bison-2.7.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/bison/bison-2.7.tar.gz'
+  sha1 'aa4f5aa51ee448bac132041df0ce25a800a3661c'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
